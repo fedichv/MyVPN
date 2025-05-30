37c0cd6f9cc3d63c008ebaa57b5cbedb964ac8a4
fedichv <fedichev06@bk.ru>
2025-05-30 19:18:11 +0300

    first commit

---
+++ MyVPN/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyVPN
+//
+//  Created by Владимир Федичев on 5/30/25.
+//
+
+import UIKit
+import TunnelKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
